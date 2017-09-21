35a2742cb42086334e9af28b09413078b605c816
georgecode <georgeczurlanis@gmail.com>
2017-09-21 01:05:24 -0700

    Add working material_ui button example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+// import AppBarExampleIcon from './components/navBar/navBar'
 import logo from './logo.svg';
 import './App.css';
+// import AppBarExampleIcon from './components/navBar/navBar'
+
+
+
+// import AddTodo from './components/addTodo/';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <RaisedButton label="Material UI Button" />
+
       </div>
+      </MuiThemeProvider>
+
     );
   }
 }
